0ca64500afce026f3d64ab3f44d82c10dd2555f8
Farhan Asghar <62025759+farhan523@users.noreply.github.com>
2024-05-19 09:58:00 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,120 +1,122 @@
-const http = require("http")
-const Socket = require("websocket").server
-const server = http.createServer(()=>{})
+const http = require("http");
+const Socket = require("websocket").server;
+const PORT = process.env.PORT || 3000;
+const server = http.createServer(() => {});
 
-server.listen(3000,()=>{
-    console.log(`Server Running At 3000`)
-})
+server.listen(PORT, () => {
+    console.log(`Server Running at ${PORT}`);
+});
 
-const webSocket = new Socket({httpServer:server})
-const users = []
-webSocket.on('request',(req)=>{
-    const connection = req.accept()
+const webSocket = new Socket({ httpServer: server });
+const users = [];
 
-    connection.on('message',(message)=>{
-        const data = JSON.parse(message.utf8Data)
-        console.log(data);
-        const user = findUser(data.name)
-       
-        switch(data.type){
-            case "store_user":
-                if(user !=null){
-                    //our user exists
-                    connection.send(JSON.stringify({
-                        type:'user already exists'
-                    }))
-                    
-                    return
+webSocket.on('request', (req) => {
+    const connection = req.accept();
 
-                }
+    connection.on('message', (message) => {
+        try {
+            const data = JSON.parse(message.utf8Data);
+            console.log(data);
+            const user = findUser(data.name);
 
-                const newUser = {
-                    name:data.name, conn: connection
-                }
-                users.push(newUser)
-            break
+            switch (data.type) {
+                case "store_user":
+                    if (user !== null) {
+                        connection.send(JSON.stringify({
+                            type: 'user already exists'
+                        }));
+                        return;
+                    }
 
-            case "start_call":
-                let userToCall = findUser(data.target)
-
-                if(userToCall){
-                    connection.send(JSON.stringify({
-                        type:"call_response", data:"user is ready for call"
-                    }))
-                } else{
-                    connection.send(JSON.stringify({
-                        type:"call_response", data:"user is not online"
-                    }))
-                }
-
-            break
-            
-            case "create_offer":
-                let userToReceiveOffer = findUser(data.target)
-
-                if (userToReceiveOffer){
-                    userToReceiveOffer.conn.send(JSON.stringify({
-                        type:"offer_received",
-                        name:data.name,
-                        data:data.data.sdp
-                    }))
-                }
-            break
-                
-            case "create_answer":
-                let userToReceiveAnswer = findUser(data.target)
-                if(userToReceiveAnswer){
-                    userToReceiveAnswer.conn.send(JSON.stringify({
-                        type:"answer_received",
+                    const newUser = {
                         name: data.name,
-                       data: data.data ? data.data.sdp : null
+                        conn: connection
+                    };
+                    users.push(newUser);
+                    break;
 
-                    }))
-                }
-            break
+                case "start_call":
+                    let userToCall = findUser(data.target);
 
-            case "ice_candidate":
-                let userToReceiveIceCandidate = findUser(data.target)
-                if(userToReceiveIceCandidate){
-                    userToReceiveIceCandidate.conn.send(JSON.stringify({
-                        type:"ice_candidate",
-                        name:data.name,
-                        data:{
-                            sdpMLineIndex:data.data.sdpMLineIndex,
-                            sdpMid:data.data.sdpMid,
-                            sdpCandidate: data.data.sdpCandidate
-                        }
-                    }))
-                }
-            break
+                    if (userToCall) {
+                        connection.send(JSON.stringify({
+                            type: "call_response",
+                            data: "user is ready for call"
+                        }));
+                    } else {
+                        connection.send(JSON.stringify({
+                            type: "call_response",
+                            data: "user is not online"
+                        }));
+                    }
+                    break;
 
-            case "end_call":
-                let userToNotifyEndCall = findUser(data.target);
-                if (userToNotifyEndCall) {
-                    userToNotifyEndCall.conn.send(JSON.stringify({
-                        type: "call_ended",
-                        name: data.name
-                    }));
-                }
-            break
+                case "create_offer":
+                    let userToReceiveOffer = findUser(data.target);
 
+                    if (userToReceiveOffer) {
+                        userToReceiveOffer.conn.send(JSON.stringify({
+                            type: "offer_received",
+                            name: data.name,
+                            data: data.data.sdp
+                        }));
+                    }
+                    break;
 
-        }
+                case "create_answer":
+                    let userToReceiveAnswer = findUser(data.target);
+                    if (userToReceiveAnswer) {
+                        userToReceiveAnswer.conn.send(JSON.stringify({
+                            type: "answer_received",
+                            name: data.name,
+                            data: data.data ? data.data.sdp : null
+                        }));
+                    }
+                    break;
 
-    })
-    
-    connection.on('close', () =>{
-        users.forEach( user => {
-            if(user.conn === connection){
-                users.splice(users.indexOf(user),1)
+                case "ice_candidate":
+                    let userToReceiveIceCandidate = findUser(data.target);
+                    if (userToReceiveIceCandidate) {
+                        userToReceiveIceCandidate.conn.send(JSON.stringify({
+                            type: "ice_candidate",
+                            name: data.name,
+                            data: {
+                                sdpMLineIndex: data.data.sdpMLineIndex,
+                                sdpMid: data.data.sdpMid,
+                                sdpCandidate: data.data.sdpCandidate
+                            }
+                        }));
+                    }
+                    break;
+
+                case "end_call":
+                    let userToNotifyEndCall = findUser(data.target);
+                    if (userToNotifyEndCall) {
+                        userToNotifyEndCall.conn.send(JSON.stringify({
+                            type: "call_ended",
+                            name: data.name
+                        }));
+                    }
+                    break;
             }
-        })
-    })
-})
+        } catch (error) {
+            console.error('Invalid message format', error);
+        }
+    });
 
-const findUser = username =>{
-    for(let i=0; i<users.length;i++){
-        if(users[i].name === username)
-        return users[i]
+    connection.on('close', () => {
+        users.forEach((user, index) => {
+            if (user.conn === connection) {
+                users.splice(index, 1);
+            }
+        });
+    });
+});
+
+const findUser = (username) => {
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].name === username)
+            return users[i];
     }
-}
+    return null;
+};
